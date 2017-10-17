eb20726ea72dc7dcef1b2b8baf9e2b07f53efc97
john.george <john.george@claranet.uk>
2017-10-17 15:55:38 +0100

    Added host writes to indicate test status

--- Test-Audit.ps1
+++ Test-Audit.ps1
@@ -1,11 +1,14 @@
 # Build test script using vagrant 2008 R2 box
 
 # Make sure the vagrant box is up
+Write-Host "Running command 'vagrant up' on 'windows-audit' solution" -ForegroundColor Yellow;
 Invoke-Expression "vagrant up";
 
 # Get our PSCredential in scope
+Write-Host "Building credentials" -ForegroundColor Yellow;
 $Password = "vagrant" | ConvertTo-SecureString -AsPlainText -Force;
 $PSCredential = New-Object System.Management.Automation.PSCredential("vagrant",$Password);
 
 # Execute our script against the vagrant box
+Write-Host "Invoking test" -ForegroundColor Yellow;
 .\Invoke-WindowsAudit.ps1 -Computers "127.0.0.1:55985" -PSCredential $PSCredential;
