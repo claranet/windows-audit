5f0ee8101ac8a137213749569289cf48655fd0a0
john.george <john.george@claranet.uk>
2017-10-17 11:34:52 +0100

    Added compute, networking, storage and completed scaffolding

--- Invoke-WindowsAudit.ps1
+++ Invoke-WindowsAudit.ps1
@@ -19,15 +19,63 @@
 
 [CmdletBinding()]
 Param( 
-    
+    #
 )
 
 #---------[ Declarations ]---------
+
 # EAP to stop so we can trap errors in catch blocks
 $ErrorActionPreference = "Stop";
 
+# Get our return object sorted out
+$HostInformation = New-Object PSCustomObject;
+
 #---------[ Functions ]---------
-#Functions
+
+# Easy add-member function
+Function Add-HostInformation {
+    [Cmdletbinding()]
+    Param(
+        # The name of the property we're adding
+        [Parameter(Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [String]$Name,
+
+        # The value of the property we're adding
+        [Parameter(Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [Object]$Value
+    )
+
+    # Add the property to HostInformation
+    $HostInformation | Add-Member -MemberType NoteProperty -Name $Name -Value $Value;
+
+}
 
 #---------[ Main() ]---------
-#Main()
+
+# Compute
+Add-HostInformation -Name Compute -Value $(Get-WMIObject -Class "Win32_Processor");
+
+# Memory
+Add-HostInformation -Name Memory -Value $(Get-WMIObject -Class "Win32_PhysicalMemory");
+
+# Storage
+Add-HostInformation -Name Storage -Value $(New-Object PSCustomObject -Property @{
+    PhysicalDisks = $(Get-WMIObject -Class "Win32_DiskDrive")
+    LogicalDisks  = $(Get-WMIObject -Class "Win32_LogicalDisk")
+    Volumes       = $(Get-WMIObject -Class "Win32_Volume")
+    SharedFolders = "TODO"
+    MountedDrives = $(Get-WMIObject -Class "Win32_MountPoint")
+});
+
+# Networking
+
+# Peripherals
+
+# Applications and features
+
+# System information
+
+#---------[ Return ]---------
+return $HostInformation;
