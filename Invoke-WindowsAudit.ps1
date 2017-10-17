e025e8ab2542b16a740b6d60f1a98346d3447cf0
john.george <john.george@claranet.uk>
2017-10-17 13:45:46 +0100

    Added peripherals, applications/features, sysinfo
    Worked out a bit more towards the gathering, parsing ipconfig /all turned out to be a bust unfortunately. Now need to
    
    check the spreadsheet and refine what is being gathered

--- Invoke-WindowsAudit.ps1
+++ Invoke-WindowsAudit.ps1
@@ -70,12 +70,30 @@ Add-HostInformation -Name Storage -Value $(New-Object PSCustomObject -Property @
 });
 
 # Networking
+Add-HostInformation -Name Networking -Value $(New-Object PSCustomObject -Property @{
+    AdapterInformation = $(Get-WMIObject -Class "Win32_NetworkAdapterConfiguration")
+    Hostname           = $env:COMPUTERNAME
+    NTPConfiguration   =  $(Invoke-Expression "w32tm /query /configuration")
+    Firewall           = "TODO, as get-netfirewallrule is fairly new"
+});
 
 # Peripherals
+Add-HostInformation -Name Peripherals -Value $(New-Object PSCustomObject -Property @{
+    USBDevices    = $(Get-WMIObject -Class "Win32_USBControllerDevice")
+    SerialDevices = $(Get-WMIObject -Class "Win32_SerialPort")
+    Printers      = $(Get-WMIObject -Class "Win32_Printer")
+})
 
 # Applications and features
+Add-HostInformation -Name Applications -Value $(New-Object PSCustomObject -Property @{
+    x32 = $(Get-ChildItem "HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*")
+    x64 = $(Get-ChildItem "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*")
+})
+
+Add-HostInformation -Name RolesAndFeatures -Value $(Get-WindowsFeature);
 
 # System information
+Add-HostInformation -Name SystemInformation -Value $(Invoke-Expression "systeminfo");
 
 #---------[ Return ]---------
 return $HostInformation;
