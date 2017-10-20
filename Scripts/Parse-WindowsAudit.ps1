bd2f62ed24eca7a890ab95cd1f331ad5006d0a8c
john.george <john.george@claranet.uk>
2017-10-20 19:53:27 +0100

    Added initial content to Parse-WindowsAudit.ps1

--- Scripts/Parse-WindowsAudit.ps1
+++ Scripts/Parse-WindowsAudit.ps1
@@ -1,34 +1,66 @@
 <#
     .SYNOPSIS
-    Name: Parse-WindowsAudit.ps1
-    Gathers information about a Windows Server instance
+    Name: Parse-WindowsAuditData.ps1
+    Parses the results from Get-WindowsAuditData.ps1
     
     .DESCRIPTION
-    This script will gather a variety of information from a Windows Server instance,
-    returning it in a format that can be manipulated to produce an output report
-    indicating the machine's current Application/Hardware/Configuration status.
+    This script is a thin wrapper to provide ease of use for the filters in
+    this solution. The filters provide a way to parse the same data repeatedly
+    to create different views of the data, this will validate that the filter
+    chosen is valid and call the filter multiple times providing each file from
+    the RawData directory.
 
-    .PARAMETER Computers [String[]]
-    String array of computers to run this script on. Defaults to this computer
-    if not specified. If the computer value is a [host:port] or [ip:port] combination
-    the specified port will be used for WinRM.
-
-    .PARAMETER PSCredential [PSCredential]
-    PSCredential that will be used for WinRM communications. Must be valid
-    on the machines you're trying to connect to.
-    
     .EXAMPLE
-    .\Invoke-WindowsAudit.ps1
-    This will execute the script on the current computer using the current user's
-    identity.
+    .\Parse-WindowsAuditData.ps1 -Filter Example
+    This will parse the entire contents of the RawData directory, generating
+    the output CSV files for compliation using the 'Example' filter.
 
-    .\Invoke-WindowsAudit.ps1 -Computers "dev-test-01","dev-test-02" -PSCredential $MyPSCredential
-    This will execute the script on both of the named computers, using the specified
-    PSCredential from the $MyPSCredential variable.
+    .TODO# Need to add error handling and output logging
 
-    .\Invoke-WindowsAudit.ps1 -Computers "dev-test-01","192.168.0.10:55876","dev-test-06:443"
-    This will execute the script on all three of the named computers, using the
-    specified ports for WinRM on latter two and the current user's identity.
-
-    #requires -version 2
+    #requires -version 5
 #>
+
+# Dynamic parameter so we can autocomplete the filters
+DynamicParam {       
+    # Create the attribute collection
+    $AttributeCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute];
+    
+    # Create and set the attributes
+    $ParameterAttribute = New-Object System.Management.Automation.ParameterAttribute;
+    $ParameterAttribute.Mandatory = $False;
+
+    # Add the attributes to the attributes collection
+    $AttributeCollection.Add($ParameterAttribute);
+        
+    # Create the dictionary 
+    $RuntimeParameterDictionary = New-Object System.Management.Automation.RuntimeDefinedParameterDictionary;
+                
+    # Generate and set the ValidateSet 
+    $ArraySet = (Get-ChildItem -Path "..\Filters" -File | Select-Object -ExpandProperty Name).Replace(".ps1","");
+    $ValidateSetAttribute = New-Object System.Management.Automation.ValidateSetAttribute($ArraySet);
+
+    # Add the ValidateSet to the attributes collection
+    $AttributeCollection.Add($ValidateSetAttribute);
+        
+    # Create and add the dynamic param to the dict
+    $RuntimeParameter = New-Object System.Management.Automation.RuntimeDefinedParameter("Filter", [string], $AttributeCollection);
+    $RuntimeParameterDictionary.Add("Filter", $RuntimeParameter);
+}
+
+Process {
+    # Get the filter name from the dynamic parameter
+    $FilterName = $PSBoundParameters["Filter"];
+    $FilterPath = ".\Filters\$FilterName.ps1";
+
+    # Get a list of the CLI XML files to process
+    $CliXmlFilesToProcess = Get-ChildItem ".\RawData\*cli.xml";
+
+    # Enumerate the collection
+    $CliXmlFilesToProcess | %{
+        # Get the CLI XML back into a PSCustomObject
+        $HostInformation = Import-Clixml -Path $_.FullName;
+
+        # And pass the result on to the correct filter for execution
+        & $FilterPath -HostInformation $HostInformation;
+    }
+}
