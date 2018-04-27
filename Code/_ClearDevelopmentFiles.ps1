a4833f2c7f7ccc9444c9bfc650b82817bf33efc5
john.george <john.george@claranet.uk>
2018-04-27 11:11:39 +0100

    Added prebuild task script to clear out development files
    Rather a nuisance to have to keep deleting any files created during testing, script takes care of this.

---
+++ Code/_ClearDevelopmentFiles.ps1
@@ -0,0 +1,21 @@
+[Cmdletbinding()]
+Param(
+    [Parameter(Mandatory=$True)]
+    [ValidateScript({Test-Path $_})]
+    [String]$WorkspaceFolder
+)
+
+# Build the root path
+$RootPath = "{0}\Code\Audit" -f $WorkspaceFolder;
+
+# Enumerate the folders we want to clear
+"Credentials","Data","Encryption","Hosts","Results" | %{
+
+    # Build the folder path
+    $Folder = "{0}\{1}" -f $RootPath,$_;
+
+    # Enumerate all the files and remove them
+    Get-ChildItem -Path $Folder -Recurse | %{
+        Remove-Item $_.FullName -Force;
+    }
+}
