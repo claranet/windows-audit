bb95f941b5daf32ab094eaffd5a0f19389ff0e99
john.george <john.george@claranet.uk>
2018-05-09 16:33:09 +0100

    Updated Linux connection check to return more detailed output

--- Code/Audit/PowerShell/Collectors/SSH/_ConnectionCheck.sh
+++ Code/Audit/PowerShell/Collectors/SSH/_ConnectionCheck.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-# Get the properties we want
-SYSTEMUNAME=$(uname -a)
+# Echo our starter
+echo '{'
 
-# And return
-echo '{"uname":"'$SYSTEMUNAME'"}'
+# Get our original IFS and set it to \n
+IFSR=$IFS
+IFS='
+'
+# Get the main properties
+for prop in `cat /etc/*[-_]release`; do
+    KEY=${prop%%=*}
+    VAL=$(echo ${prop#*=} | sed "s/\"//g")
+    echo '"'$KEY'": "'$VAL'",'
+done
+
+# Tie off the json with the uname string and machineidentifier
+SYSTEMUNAME=$(uname -a)
+echo '"uname":"'$SYSTEMUNAME'","MachineIdentifier": ""}'
+
+# Set our IFS back to what it was
+IFS=$IFSR
